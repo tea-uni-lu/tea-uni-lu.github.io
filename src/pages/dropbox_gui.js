fb4ef9d106d2384542f9fae5063e2942f1585f70
tea-uni-lu <135242071+tea-uni-lu@users.noreply.github.com>
2023-07-16 20:36:49 +0200

    Update dropbox_gui.js

--- src/pages/dropbox_gui.js
+++ src/pages/dropbox_gui.js
@@ -1,125 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Dropbox } from 'dropbox';
-import { RiDownloadCloudLine, RiUploadCloudLine } from 'react-icons/ri';
+import React, { useState } from 'react';
+import DropboxChooser from 'react-dropbox-chooser';
 
-const CLIENT_ID = 'c0mpqunklb6vt5s';
-const CLIENT_SECRET = 'bfyvai1f20zz5ry';
-const REDIRECT_URI = 'localhost:3000/data_page';
-const ACCESS_TOKEN = 'sl.BiSW_RjCv_OJWxhCQX2HXmNqFfdvzV1UD13JompK5Z2hJ4RlVD4nSqykul2OOCFvDLPELARggufWC7Vl3AS5eAV1bHscrxzbd5_bdicAI_Jl-pdJZYX11KQ7sPjKHvW2b0wFHr0';
-const FOLDER_PATH = '/TEA2023/TEST/'
+const FOLDER_PATH = '/TEA2023/TEST/';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
-const dropbox = new Dropbox({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET, accessToken: ACCESS_TOKEN});
+function DropboxData() {
+  function handleSuccess (files){
+    console.log(files);
+  }
 
-
-function DropboxGUI() {
-  const [folderContent, setFolderContent] = useState([]);
-
-    const fetchFolderContent = async () => {
-      try {
-        const { result } = await dropbox.filesListFolder({ path: FOLDER_PATH });
-        setFolderContent(result.entries);
-      } catch (error) {
-        console.error('Error fetching folder content:', error);
-      }
-    };
-
-  useEffect(() => {
-    fetchFolderContent();
-  }, []);
-
-  const handleDownload = (path) => {
-    const downloadFile = async () => {
-      try {
-        const response = await dropbox.filesDownload({ path });
-        const downloadUrl = URL.createObjectURL(response.result.fileBlob);
-        const link = document.createElement('a');
-        link.href = downloadUrl;
-        link.download = response.result.name;
-        link.click();
-      } catch (error) {
-        console.error('Error downloading file:', error);
-      }
-    };
-
-    downloadFile();
-  };
-
-  const handleUpload = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = async (event) => {
-        const contents = event.target.result;
-        const originalFileName = file.name;
-        const uniquePath = await generateUniquePath(FOLDER_PATH, originalFileName);
-  
-        try {
-          await dropbox.filesUpload({
-            path: uniquePath,
-            contents: contents,
-            mode: { '.tag': 'add' },
-            autorename: false,
-          });
-          console.log('File uploaded successfully!');
-          fetchFolderContent();
-        } catch (error) {
-          console.error('Error uploading file:', error);
-          if (error.response && error.response.status === 400) {
-            console.log('Error details:', error.response.data);
-          }
-        }
-      };
-  
-      reader.readAsArrayBuffer(file);
-    }
-  };
-  
-  
-  const generateUniquePath = async (basePath, fileName, timestamp = Date.now()) => {
-    const newPath = `${basePath}/${timestamp}_${fileName}`;
-    try {
-      await dropbox.filesGetMetadata({ path: newPath });
-      // If metadata is found, it means the path already exists, so recursively call the function
-      return generateUniquePath(basePath, fileName, timestamp + 1);
-    } catch (error) {
-      // If metadata is not found, it means the path is unique, so return the path
-      return newPath;
-    }
-  };
-  
-  
   return (
-    <div>
-      <h2>Welcome to the TEA2023 data sharing platform!</h2>
-      {folderContent.length > 0 ? (
-        <div>
-          <h4>Contents of the Dropbox Folder:</h4>
-          <ul>
-            {folderContent.map((item) => (
-              <li key={item.id}>
-              {item.name}{' '}
-                <a
-                  href="#"
-                  onClick={() => handleDownload(item.path_lower)}
-                >
-                  <RiDownloadCloudLine />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>No content in the folder.</p>
-      )}
+    <div className="app">
+      <h3 style={{textAlign:"center"}}>Welcome to the Tea2023 data sharing platform</h3>
+      <br/> <br/>
       <div>
-        <h4>Upload to the Dropbox Folder:</h4>
-        <input type="file" onChange={handleUpload} />
-        <label htmlFor="file-upload">
-          <RiUploadCloudLine /> Upload File {/* Add the upload icon */}
-        </label>
+        <DropboxChooser appKey={CLIENT_ID}
+          success={handleSuccess}
+          cancel={()=> console.log("closed")}
+          multiselect={true}
+        >
+        </DropboxChooser>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DropboxGUI;
+export default DropboxData;
